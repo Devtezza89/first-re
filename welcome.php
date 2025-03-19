99ab6555106443d014a183653e0d0a0f92df450f
Ronoh <124288997+Swipes-dot@users.noreply.github.com>
2025-03-19 13:41:31 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -19,6 +19,7 @@ if (isset($_GET['logout'])) {
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
