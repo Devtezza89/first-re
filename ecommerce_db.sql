ee8ba9db56eabc37710c37879d01236434e61fba
Ronoh <124288997+Swipes-dot@users.noreply.github.com>
2025-03-21 12:28:42 +0300

    Update ecommerce_db.sql

--- ecommerce_db.sql
+++ ecommerce_db.sql
@@ -1,10 +1,15 @@
+DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 USE ecommerce_db;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(15) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    remember_token VARCHAR(32) NULL,
+    reset_token VARCHAR(6) NULL,
+    reset_token_expires DATETIME NULL
 );
